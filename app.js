8cdb0260c0e17095385ba3dcef3d0162f75871df
vaagee <baagii.monhbat@gmail.com>
2021-04-29 20:16:50 +0800

    ehnii hicheel.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах, 1r toglogvh 0 , 2r toglogch 1 gj temdeglene
-var activePlayer = 0;
-var deactivePlayer= 1;
+var activePlayer = 1;
 
 
 //toglogchiin eekjiin oniig hadgalah
@@ -12,14 +11,13 @@ var roundScore= 0;
 
 
 //shoo ali talaaram buusniig hadagalah function 1-6 ali  buusniig hadgalah 
-var dice = Math.floor(Math.random() * 6)+1;
+var diceNumber = Math.floor(Math.random() * 6)+1;
 
-window.document.querySelector(`#score-0`).textContent=0;
-
-window.document.querySelector(`#score-1`).textContent=0;
-
-window.document.querySelector(`#current-0`).textContent=0;
-window.document.querySelector(`#current-1`).textContent=0;
+document.getElementById("#score-0").textContent= 0;
+window.document.querySelector("#score-0").textContent=0;
+window.document.querySelector("#score-1").textContent=0;
+window.document.querySelector("#current-0").textContent=0;
+window.document.querySelector("#current-1").textContent=0;
 
 
 document.querySelector(`.dice`).style.display = "none";
